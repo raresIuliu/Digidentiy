828b30c9079110a644638226617b077d2abb2be8
raresIuliu <43181170+raresIuliu@users.noreply.github.com>
2018-09-11 19:41:49 +0300

    Add files via upload

---
+++ Digidentity/Digidentity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digidentity
+//
+//  Created by Rares Tamas on 9/11/18.
+//  Copyright © 2018 Rares Tamas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
